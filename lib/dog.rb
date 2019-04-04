e92f649cdb6258980c52a919176117183700cef7
Nissan Holzer <nissan.holzer@gmail.com>
2019-04-04 01:14:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,8 +44,11 @@ class Dog
     new_dog
   end 
   
-  def self.new_from_db 
-    
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end 
   
   def find_by_id (x)

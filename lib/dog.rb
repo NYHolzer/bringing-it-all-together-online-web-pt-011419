c86d8acc25819b7197f818d0ad3ec50dff59a878
Nissan Holzer <nissan.holzer@gmail.com>
2019-04-04 01:05:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,7 +33,7 @@ class Dog
     SQL
     
     DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute(
+    @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
   end 
 
 end 
